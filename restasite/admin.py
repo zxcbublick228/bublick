f4e335e97b69655b008e888dd2d8d846fd804df7
GGtysh <zxcbublick25@gmail.com>
2022-11-16 16:29:05 +0300

    1 commit

---
+++ restasite/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from restasite.models import MenuItem
+
+# Register your models here.
+admin.site.register(MenuItem)
---
+++ restasite/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=70, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='images', verbose_name='Изображение')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена', default=0)
+
+    TYPE = [
+        ('BRK', 'Завтрак'),
+        ('LUN', 'Обед'),
+        ('DIN', 'Ужин'),
+    ]
+
+    type = models.CharField(choices=TYPE, max_length=3, default='BRK', verbose_name='Тип')
+
+    def __str__(self):
+        return self.title
